f15a5d39b9cb8dd4c7609b1304da8d4a78f70895
Ricardo Mendes <rokusu@gmail.com>
2015-10-17 08:30:39 +0100

    Tweaks JavaScript to render demo HTML

--- tom.js
+++ tom.js
@@ -1,16 +1,24 @@
-$(document).ready(function(){
-  $('.template, .json').change(function() {
-    var e, html, json, template;
-    try {
-      template = $('.template').val();
-      json = $.parseJSON($('.json').val());
-      html = Mustache.to_html(template, json).replace(/^\s*/mg, '');
-    } catch (_error) {
-      e = _error;
-      html = e.toString();
-    }
-    return $('pre code').each(function(i, block) {
-      hljs.highlightBlock(block);
-    });
+'use strict';
+
+var renderTemplate = function() {
+  var e, html, json, template;
+  try {
+    template = $('.template').val();
+    json = $.parseJSON($('.json').val());
+    html = Mustache.to_html(template, json).replace(/^\s*/mg, '');
+  } catch (_error) {
+    e = _error;
+    html = e.toString();
+  }
+
+  $('.html').text(html);
+  return $('pre.html').each(function(i, block) {
+    hljs.highlightBlock(block);
   });
+}
+
+$(function(){
+  hljs.initHighlightingOnLoad();
+  renderTemplate();
+  $('.template, .json').keyup(renderTemplate);
 });
